7961413e0ebfa77570f19b8d0a648b2f1fc9a82e
Joona Gynther <joona@gynther.xyz>
2022-07-25 15:20:30 +0300

    v2 with progress bar to indicate active status

--- coffee.ps1
+++ coffee.ps1
@@ -1,16 +1,22 @@
+Clear-Host
+
 $wsh = new-object -ComObject WScript.Shell
 
 write-host "Pouring a cup of coffee... (keeping you(r pc) awake)"
+write-host "Exit with CTRL+C `n"
 
 $remarks = "Taking a sip...", "Taking a sip...", "Taking a sip...", "Taking a sip...", "Taking a sip...", "Taking a sip...", 
 "Taking another sip...", "Gulping a sip...", "Pouring more coffee...", "More sips...", "Still drinking coffee...", "Nope, still not tea...", "Wait is this coffee?"
+$max = 25
 
 while (1) {
   $wsh.SendKeys('+{F15}')
 
-  $time = get-date -format "HH:mm:ss"
   $remark = get-random $remarks
-  write-host "[$time] $remark"
 
-  start-sleep -seconds 59
+  for (($i = 0); $i -lt 30; $i++) {
+    $progress = "[$("#" * $i)$("." * (29 - $i) )] $remark $(" " * ($max - $remark.length))"
+    write-host -nonewline "`r$progress"
+    start-sleep -seconds 2
+  }  
 }
